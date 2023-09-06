d30358bf34844c695c33ef3337c603e0697e4e68
Naruyoshimi <83273175+Naruyoshimi@users.noreply.github.com>
2023-09-06 17:37:01 +0900

    20230906fin

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import "./styles.css";  
+import "./styles.css";
 import Map from "./Map";
+import getCSV from "./getCSV";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
         <h1>Hello CodeSandbox</h1>
         <h2>Start editing to see some magic happen!</h2>
         <Map />
+        <getCSV />
       </div>
     </>
   );
